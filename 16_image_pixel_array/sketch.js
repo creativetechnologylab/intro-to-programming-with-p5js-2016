66cd8aa3382c08d10d57566d22915f7427f6f1db
garethfoote <gareth.foote@gmail.com>
2016-11-13 14:20:24 +0000

    Using P5.Image.pixels[] instead of global/canvas pixels[]

--- 16_image_pixel_array/sketch.js
+++ 16_image_pixel_array/sketch.js
@@ -1,6 +1,6 @@
 /*
  * Image pixel array
- * @description Looping through pixels of canvas/image
+ * @description Manipulating images. Looping through and modifying pixels of image
  * @author Gareth Foote
  */
 
@@ -16,23 +16,26 @@ function setup() {
 }
 
 function draw() {
-  tint(255, 0, 0);
+  tint(255, 0, 255);
   image(img, 0, 0);
 
   // https://p5js.org/reference/#/p5/filter
   // filter("THRESHOLD");
 
-  // loadPixels();
-  // for (var i = 0; i < pixels.length; i+=4) {
-  //   var r = pixels[i];
-  //   var g = pixels[i+1];
-  //   var b = pixels[i+2];
-  //   var a = pixels[i+3];
-
-  //   pixels[i] = r;
-  //   pixels[i+1] = g;
-  //   pixels[i+2] = b;
-  //   pixels[i+3] = 255;
+  // img.loadPixels();
+  // for (var i = 0; i < img.pixels.length; i+=4) {
+  //   // Read the r,g,b & alpha values for each pixel.
+  //   var r = img.pixels[i];
+  //   var g = img.pixels[i+1];
+  //   var b = img.pixels[i+2];
+  //   var a = img.pixels[i+3];
+  //   // Modify the r,g,b & alpha values for each pixel.
+  //   img.pixels[i] = r;
+  //   img.pixels[i+1] = 0;
+  //   img.pixels[i+2] = 0;
+  //   img.pixels[i+3] = 255;
   // }
-  // updatePixels();
+  // img.updatePixels();
+  // // Rendering it back onto the canvas.
+  // image(img, 0, 0);
 }
