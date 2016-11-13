809cdb07b46db6adf7aed1b3c86968a4a4e5102f
garethfoote <gareth.foote@gmail.com>
2016-11-13 20:34:08 +0000

    Loading and animating images

--- 17_loading_images/sketch.js
+++ 17_loading_images/sketch.js
@@ -12,24 +12,28 @@ var img4;
 function setup() {
   createCanvas(500, 500);
   img1 = loadImage("images/trump.jpg");
+  img2 = loadImage("images/clinton.jpg");
+  img3 = loadImage("images/farage.jpg");
+  img4 = loadImage("images/corbyn.jpg");
+  frameRate(2);
 }
 
 function draw() {
   clear();
-  // Top left.
-  if(mouseX < width/2 && mouseY < height/2){
-    tint(255, 127);
-  } else {
-    tint(255);
+
+  // '%' = Modulus
+  if(frameCount%4 == 0){ // Divisible by 4 exactly
+    image(img1, 0, 0, width/2, height/2);
+  } else if(frameCount%4 == 1){ // Divisible by 4 with remainder 1
+    image(img2, 0, 0, width/2, height/2);
+  } else if(frameCount%4 == 2){ // Divisible by 4 with remainder 2
+    image(img3, 0, 0, width/2, height/2);
+  } else if(frameCount%4 == 3){ // Divisible by 4 with remainder 3
+    image(img4, 0, 0, width/2, height/2);
   }
-  image(img1, 0, 0, width/2, height/2);
 
   // Transparent fill for the rectangle.
   fill(0, 0);
   strokeWeight(2);
   rect(0, 0, width/2, height/2);
-
-  // Task: Top right.
-  // Task: Bottom left.
-  // Task: Bottom right.
 }
